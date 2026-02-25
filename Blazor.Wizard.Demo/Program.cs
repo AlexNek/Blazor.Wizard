2a54d50c0f0e1dc0654ea245b64715b25f7bcdb6
AlexNek <alexnek@russinger.com>
2026-02-25 23:05:26 +0100

    add general logging

--- Blazor.Wizard.Demo/Program.cs
+++ Blazor.Wizard.Demo/Program.cs
@@ -1,16 +1,23 @@
+using System.Diagnostics;
+
 using Blazor.Wizard.Demo;
-using Blazor.Wizard.Demo.Components;
 
 using Serilog;
-using Serilog.Settings.Configuration;
+using Serilog.Debugging;
+
+SelfLog.Enable(msg => Trace.WriteLine($"SERILOG DEBUG: {msg}"));
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("serilog.json", optional: false, reloadOnChange: true);
 
 // Configure Serilog from serilog.json
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 builder.Host.UseSerilog();
+
+Log.Information("***Application starting...***");
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -21,10 +28,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Error", createScopeForErrors: true);
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
@@ -34,4 +42,5 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+Log.Information("Application started successfully");
 app.Run();
